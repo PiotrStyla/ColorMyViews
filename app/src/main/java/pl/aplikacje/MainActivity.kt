f332982405232e53a11a352aea30b5e609728441
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-07-22 16:27:28 +0200

    2

--- app/src/main/java/pl/aplikacje/MainActivity.kt
+++ app/src/main/java/pl/aplikacje/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,14 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundResource(R.drawable.logo_hip)
-            R.id.box_three_text -> view.setBackgroundResource(R.drawable.orange)
-            R.id.box_four_text -> view.setBackgroundResource(R.drawable.t_o_plakatu)
-            R.id.box_five_text -> view.setBackgroundResource(R.drawable.logotelefon1024)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_five_text -> view.setBackgroundColor(Color.CYAN)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
-            else -> view.setBackgroundResource(R.drawable.ciuchy)
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 
@@ -37,10 +41,14 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+
         val clickableViews: List<View> =
-            listOf(
-                boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
